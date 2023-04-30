d2b6dea898ce856a9a63ab75d315568c76960a5d
sinceseptember4 <sinceseptember4@gmail.com>
2023-04-30 11:51:04 +0900

    graph

--- src/components/graph.tsx
+++ src/components/graph.tsx
@@ -1,4 +1,4 @@
-import React, { useRef,  } from "react";
+import React, { useRef } from "react";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 import accessibility from 'highcharts/modules/accessibility';
@@ -6,7 +6,7 @@ import accessibility from 'highcharts/modules/accessibility';
 
 export const Graph = (props :{data:Highcharts.Options}) => {
   accessibility(Highcharts);
-const chartComponentRef = useRef<HighchartsReact.RefObject>(null);
+  const chartComponentRef = useRef<HighchartsReact.RefObject>(null);
   return <>
       <HighchartsReact
         highcharts={Highcharts}
